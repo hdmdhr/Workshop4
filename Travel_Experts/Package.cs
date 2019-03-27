972462375863d390ef3fe6722f83bab4826cd8d4
Louise Acosta <44888474+louiseanacosta@users.noreply.github.com>
2019-03-27 17:24:52 -0600

    Revert "commit"
    This reverts commit e7180749307bca385a5b7d51389d0eedd538fac2.

--- Travel_Experts/Package.cs
+++ Travel_Experts/Package.cs
@@ -1,9 +1,4 @@
-﻿/**
- * Author: : Louise Acosta
- * Date: March 27, 2019
- **/
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,12 +8,6 @@ namespace Travel_Experts
 {
     public class Package
     {
-        public int PackageId { get; set; }
-        public string PkgName { get; set; }
-        public DateTime? PkgStartDate { get; set; }
-        public DateTime? PkgEndDate { get; set; }
-        public string PkgDesc { get; set; }
-        public decimal? PkgBasePrice { get; set; }
-        public decimal? PkgAgencyCommission { get; set; }
+         
     }
 }
