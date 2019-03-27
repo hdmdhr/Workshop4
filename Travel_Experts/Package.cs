e7180749307bca385a5b7d51389d0eedd538fac2
Louise Acosta <44888474+louiseanacosta@users.noreply.github.com>
2019-03-27 17:22:21 -0600

    commit

--- Travel_Experts/Package.cs
+++ Travel_Experts/Package.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/**
+ * Author: : Louise Acosta
+ * Date: March 27, 2019
+ **/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +13,12 @@ namespace Travel_Experts
 {
     public class Package
     {
-         
+        public int PackageId { get; set; }
+        public string PkgName { get; set; }
+        public DateTime? PkgStartDate { get; set; }
+        public DateTime? PkgEndDate { get; set; }
+        public string PkgDesc { get; set; }
+        public decimal? PkgBasePrice { get; set; }
+        public decimal? PkgAgencyCommission { get; set; }
     }
 }
--- Workshop4/Form1.cs
+++ Workshop4/Form1.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/**
+ * Author: : Louise Acosta
+ * Date: March 27, 2019
+ **/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +12,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Travel_Experts;
 
 namespace Workshop4
 {
     public partial class Form1 : Form
     {
+        List<Package> packages = PackageDB.GetPackages();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            packageBindingSource.DataSource = packages;
         }
     }
 }
